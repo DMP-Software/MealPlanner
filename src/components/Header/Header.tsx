d68cf09792673914d039db8db13b191b1b025bd6
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2019-01-27 19:24:45 +0100

    Add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.scss';
+
+export const Header: React.FunctionComponent = React.memo(() => {
+    return (
+      <div className='header'>
+        <Link to='/' className='header__btn btn--default'>Meal planner</Link>
+      </div>
+    )
+});
