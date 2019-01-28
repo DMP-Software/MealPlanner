4ccb2938f54e97dfdc4c81c84f91917c059fb2f2
Dominika <34394232+dominikaporzyczka@users.noreply.github.com>
2019-01-28 22:13:50 +0100

    Add react-router, add header and basic styles (#2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import { Header } from './components/Header/Header.tsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">Meal planner</div>
+      <div className="app">
+        <Header/>
+        <div>Main Page</div>
+      </div>
     );
   }
 }
