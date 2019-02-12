592427e74904c7627bf23345409688ea4f66cec2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-12 18:06:52 +0000

    Deploy DMP-Software/MealPlanner to github.com/DMP-Software/MealPlanner.git:gh-pages

---
+++ precache-manifest.ad39e627fe633726dbddaa2a44719087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8c613d7283bcf9400c4",
+    "url": "/MealPlanner/static/css/main.49b3a1b3.chunk.css"
+  },
+  {
+    "revision": "f8c613d7283bcf9400c4",
+    "url": "/MealPlanner/static/js/main.f8c613d7.chunk.js"
+  },
+  {
+    "revision": "c51f67a75dee557e3aeb",
+    "url": "/MealPlanner/static/js/1.c51f67a7.chunk.js"
+  },
+  {
+    "revision": "7016db365237396a1d69",
+    "url": "/MealPlanner/static/js/runtime~main.7016db36.js"
+  },
+  {
+    "revision": "ca2d8271b7e514543091e9985e102a9d",
+    "url": "/MealPlanner/index.html"
+  }
+];
