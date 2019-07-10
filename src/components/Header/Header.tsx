898a74504d88b45fbea604b8b29d2bbe3c772e2f
Dominika <34394232+dominikaporzyczka@users.noreply.github.com>
2019-07-10 23:01:55 +0200

    Create basic schema of application (#12)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+
 import './header.scss';
+import {
+  MAIN_PAGE_PATH,
+  MY_GOALS_PAGE_PATH,
+  PROGRESS_REPORT_PAGE_PATH,
+  MY_ACTIVITIES_PAGE_PATH
+} from '../../constants/paths';
 
 export const Header: React.FunctionComponent = React.memo(() => {
   return (
     <div className="header">
-      <Link to="/" className="header__btn btn--default">
-        Meal planner
+      <Link to={MAIN_PAGE_PATH} className="header__btn btn--default">
+        Main
+      </Link>
+      <Link to={MY_GOALS_PAGE_PATH} className="header__btn btn--default">
+        My Goals
+      </Link>
+      <Link to={MY_ACTIVITIES_PAGE_PATH} className="header__btn btn--default">
+        My Activities
+      </Link>
+      <Link to={PROGRESS_REPORT_PAGE_PATH} className="header__btn btn--default">
+        Progress
       </Link>
     </div>
   );
---
+++ src/components/pages/MyActivitiesPage/MyActivitiesPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './myActivitiesPage.scss';
+
+export const myActivitiesPage: React.FunctionComponent = () => {
+  return (
+    <div className="my-activities-page">
+      <h1 className="my-activities-page__title">My Activities</h1>
+    </div>
+  );
+};
